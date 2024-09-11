b204a03adcddef7abf19bd6466b4f1c5d147b2fd
lawb4 <yahorlabanau@gmail.com>
2024-09-11 16:55:39 +0300

    hometask1

---
+++ src/main/java/hometask1/Main.java
@@ -0,0 +1,18 @@
+package hometask1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        sum();
+        subtract();
+    }
+
+    public static void sum() {
+        int sum = 5 + 1;
+        System.out.println(sum);
+    }
+
+    public static void subtract() {
+        System.out.println(1 - 2);
+    }
+}
